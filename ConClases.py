39f356e55dff6e51ea0bc64381f13e18b2b69a1c
JaimeBallesterosCalvo <100454114@alumnos.uc3m.es>
2023-10-12 22:13:10 +0200

    hecho lo de los mensajes pero sin desencifrar bien

--- ConClases.py
+++ ConClases.py
@@ -6,6 +6,7 @@ from tkinter import ttk #proporciona el desplegable como el de experiencia
 import os
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 class Main:
     def __init__(self):
@@ -16,6 +17,7 @@ class Main:
 
     def mostrar_menu_principal(self, nombre_usuario):
         # Muestra la interfaz del menú principal
+        key = AESGCM.generate_key(bit_length=128)
         root_menu = tk.Tk()  # <-- Cambiado de self.root a root_menu
         self.menu_principal = MenuPrincipal(root_menu, self, nombre_usuario)
         root_menu.mainloop()
@@ -32,6 +34,8 @@ class MenuPrincipal:
         #Inicializa el menu
         self.master = master
         self.app = app
+        self.nombre_usuario_actual = nombre_usuario
+        self.key = AESGCM.generate_key(bit_length=128)
         self.interfaz_menu_principal(nombre_usuario)
 
     def interfaz_menu_principal(self,nombre_usuario):
@@ -68,12 +72,13 @@ class MenuPrincipal:
         pestaña_mensajes = tk.Frame(notebook)
         notebook.add(pestaña_mensajes, text="Mensajes")
 
-        # Buscador de nombres de usuario
-        self.buscador_entry = tk.Entry(pestaña_mensajes)
-        self.buscador_entry.grid(row=0, column=0, padx=10, pady=10)
+        # Cuadro de búsqueda
+        buscador_entry = tk.Entry(pestaña_mensajes)
+        buscador_entry.grid(row=0, column=1, padx=10, pady=10)
 
-        boton_buscar = tk.Button(pestaña_mensajes, text="Buscar", command=self.abrir_chat)
-        boton_buscar.grid(row=0, column=1, padx=10, pady=10)
+        # Botón de búsqueda
+        boton_buscar = tk.Button(pestaña_mensajes, text="Buscar", command=lambda: self.buscar_mensajes(buscador_entry.get()))
+        boton_buscar.grid(row=0, column=2, padx=10, pady=10)
 
     def crear_pestaña_torneos(self, notebook):
         pestaña_torneos = tk.Frame(notebook)
@@ -118,42 +123,38 @@ class MenuPrincipal:
 
         return datos_usuario
     
-    def abrir_chat(self):
-        nombre_usuario_a_buscar = self.buscador_entry.get()
-
-        # Verificar si el usuario está intentando buscarse a sí mismo
-        if nombre_usuario_a_buscar == self.obtener_datos_usuario()[4]:  # Índice 4 para obtener el nombre de usuario
-            messagebox.showinfo("Error", "No puedes buscarte a ti mismo.")
-            return
-        
-        # Realizar la búsqueda en la base de datos (reemplazar con la lógica adecuada)
-        resultado_busqueda = self.buscar_en_base_de_datos(nombre_usuario_a_buscar)
-
-        if resultado_busqueda:
-            self.abrir_ventana_chat(nombre_usuario_a_buscar)
-        else:
-            messagebox.showinfo("Resultado de la Búsqueda", "Usuario no encontrado.")
-
-    def buscar_en_base_de_datos(self, nombre_usuario):
-        # Lógica de búsqueda en la base de datos (reemplazar con la lógica adecuada)
+    def buscar_mensajes(self, nombre_usuario):
         conexion = sqlite3.connect("registro.db")
         try:
             cursor = conexion.cursor()
             cursor.execute('''
-                SELECT * FROM usuarios WHERE nombre_usuario = ?
+                SELECT 1
+                FROM usuarios
+                WHERE nombre_usuario = ?
             ''', (nombre_usuario,))
             resultado = cursor.fetchone()
+            print(f"Resultado de la búsqueda: {resultado}")
         finally:
             conexion.close()
+        
+        if resultado is not None:
+            respuesta = messagebox.askquestion("Usuario Encontrado", f"¿Quieres hablar con {nombre_usuario}?")
+            if respuesta == 'yes':
+                print(f"Iniciar conversación con {nombre_usuario}.")
+                self.iniciar_chat(nombre_usuario)
+            else:
+                print("Conversación cancelada.")
+        else:
+            messagebox.showinfo("Usuario No Encontrado", f"No se ha encontrado a {nombre_usuario}.")
 
         return resultado is not None
     
-    def abrir_ventana_chat(self, nombre_usuario):
-        # Lógica para abrir una ventana de chat (puedes personalizar según tus necesidades)
-        ventana_chat = tk.Toplevel(self.master)
-        ventana_chat.title(f"Chat con {nombre_usuario}")
-        # Aquí puedes agregar widgets y lógica para el chat
-      
+    def iniciar_chat(self, nombre_usuario):
+        # Obtener el ID del usuario actual usando el nombre de usuario
+        id_usuario_actual = self.obtener_id_usuario(self.nombre_usuario_actual)
+
+        # Abrir una nueva ventana de chat
+        chat_ventana = ChatVentana(self.master, nombre_usuario, id_usuario_actual, self.key)
 
 class InterfazInicio:
     #Creación de la interfaz inicio
@@ -189,7 +190,7 @@ class InterfazInicio:
 
     def mostrar_interfaz_registro(self):
         # Función para que pase a la interfaz de registro
-        self.pasar_interfaz_registro = InterfazRegistro(self.master, self.app)
+        self.pasar_interfaz_registro = InterfazRegistro(self.master, self.app, self)
 
     def mostrar_menu_principal(self):
         # Función para que pase al menú principal
@@ -245,9 +246,13 @@ class InterfazInicio:
     
 class InterfazRegistro:
     #Creación de la interfaz de registro
-    def __init__(self, master):
+    def __init__(self, master, app, main_app):
+        self.master = master
+        self.app = app
+        self.main_app = main_app
         self.master = tk.Toplevel(master)
         self.interfaz_registro()
+        
 
     def interfaz_registro(self):
         #Inicializa las cosas que aparecen en la interfaz de registro
@@ -374,8 +379,136 @@ class InterfazRegistro:
         conexion.commit()
         conexion.close()
 
+class ChatVentana(tk.Toplevel):
+    def __init__(self, master, nombre_usuario, id_usuario, key):
+        super().__init__(master)
+        self.title(f"Chat con {nombre_usuario}")
+        self.geometry("400x400")
+        print(f"ChatVentana creada para {nombre_usuario} (ID: {id_usuario})")
+        self.key = key #para gestionar la encriptaccion
 
+        # Atributos para gestionar la base de datos
+        self.id_usuario = id_usuario
+        self.id_destinatario = self.obtener_id_destinatario(nombre_usuario)
 
+        # Etiqueta que muestra con quién estás hablando
+        self.etiqueta_conversacion = tk.Label(self, text=f"Conversación con {nombre_usuario}")
+        self.etiqueta_conversacion.pack(pady=10)
+
+        # Área de visualización para los mensajes
+        self.area_mensajes = tk.Text(self, height=15, width=40)
+        self.area_mensajes.pack(padx=10, pady=10)
+
+        # Cuadro de texto para escribir mensajes
+        self.cuadro_mensaje = tk.Entry(self, width=40)
+        self.cuadro_mensaje.pack(padx=10, pady=5)
+
+        # Botón para enviar mensajes
+        boton_enviar = tk.Button(self, text="Enviar", command=self.enviar_mensaje)
+        boton_enviar.pack(pady=10)
+
+        self.cargar_y_mostrar_mensajes()
+
+    def cargar_y_mostrar_mensajes(self):
+        print("Cargando y mostrando mensajes...")
+        mensajes = self.obtener_mensajes_desde_bd()
+        print("Mensajes obtenidos:", mensajes)
+        if not mensajes:
+            # No hay mensajes, mostrar mensaje de bienvenida
+            self.area_mensajes.insert(tk.END, "¡Bienvenido al chat!\n")
+        else:
+            for mensaje in mensajes:
+                decrypted_message = self.decrypt_message(mensaje[0], bytes(mensaje[1]), self.key, b"additional_authenticated_data")
+                self.area_mensajes.insert(tk.END, f"{mensaje[1]}: {decrypted_message.decode('utf-8')}\n")
+    def obtener_mensajes_desde_bd(self):
+        conexion = sqlite3.connect("registro.db")
+        try:
+            cursor = conexion.cursor()
+            # Ejemplo de consulta: obtener mensajes enviados al destinatario actual
+            cursor.execute('''
+                SELECT m.contenido, m.nonce, u.nombre_usuario as nombre_emisor
+                FROM mensajes m
+                JOIN usuarios u ON m.id_emisor = u.id
+                WHERE (m.id_destinatario = ? AND m.id_emisor = ?) OR 
+                  (m.id_destinatario = ? AND m.id_emisor = ?) --Esto es para que se enseñen los mensajes solo con esa persona
+            ''', (self.id_usuario, self.id_destinatario, self.id_destinatario, self.id_usuario))
+            mensajes = cursor.fetchall()
+            if not mensajes:
+                return False
+        except sqlite3.OperationalError as e:
+            # Si hay un error de operación, por ejemplo, si la tabla no existe, imprime el error y retorna False
+            print(f"Error al obtener mensajes desde la base de datos: {e}")
+            return False
+        finally:
+            conexion.close()
+        return mensajes
+
+    def enviar_mensaje(self):
+        mensaje = self.cuadro_mensaje.get()
+        if mensaje:
+            ciphertext, nonce = self.encrypt_message(mensaje.encode("utf-8"), self.key, b"additional_authenticated_data")
+            self.area_mensajes.insert(tk.END, f"Tú: {mensaje}\n")
+            self.cuadro_mensaje.delete(0, tk.END)
+
+            self.guardar_mensaje(ciphertext, nonce)
+        
+    def guardar_mensaje(self, ciphertext, nonce):
+        conexion = sqlite3.connect("registro.db")
+        try:
+            cursor = conexion.cursor()
+
+            # Crear la tabla mensajes si no existe
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS mensajes (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id_emisor INTEGER,
+                    id_destinatario INTEGER,
+                    contenido TEXT,
+                    nonce BLOB,  -- Cambiado a BLOB para almacenar bytes
+                    FOREIGN KEY (id_emisor) REFERENCES usuarios(id),
+                    FOREIGN KEY (id_destinatario) REFERENCES usuarios(id)
+                )
+            ''')
+
+            # Insertar el mensaje en la tabla mensajes
+            cursor.execute('''
+                INSERT INTO mensajes (id_emisor, id_destinatario, contenido, nonce)
+                VALUES (?, ?, ?, ?)
+            ''', (self.id_usuario, self.id_destinatario, ciphertext, nonce))
+            conexion.commit()
+            print("Mensaje guardado en la base de datos.")
+        finally:
+            conexion.close()
+
+    def obtener_id_destinatario(self, nombre_destinatario):
+        conexion = sqlite3.connect("registro.db")
+        try:
+            cursor = conexion.cursor()
+            cursor.execute('''
+                SELECT id
+                FROM usuarios
+                WHERE nombre_usuario = ?
+            ''', (nombre_destinatario,))
+            id_destinatario = cursor.fetchone()
+        finally:
+            conexion.close()
+
+        return id_destinatario[0] if id_destinatario else None
+
+    def encrypt_message(self, message, key, aad):
+        aesgcm = AESGCM(key)
+        nonce = os.urandom(12)
+        ciphertext = aesgcm.encrypt(nonce, message, aad)
+        return ciphertext, nonce
+
+    def decrypt_message(self, ciphertext, nonce, key, aad):
+        print(f"Decryption - Nonce: {nonce}")
+        print(f"Decryption - Ciphertext: {ciphertext}")
+        print(f"Decryption - AAD: {aad}")
+
+        aesgcm = AESGCM(key)
+        decrypted_message = aesgcm.decrypt(nonce, ciphertext, aad)
+        return decrypted_message
 
 if __name__ == "__main__":
     main = Main()
--- prueba.py
+++ prueba.py
@@ -19,3 +19,15 @@ kdf = Scrypt(
     p=1,
 )
 kdf.verify(b"my great password", key)
+
+
+import os
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+data = b"a secret message"
+aad = b"authenticated but unencrypted data"
+key = AESGCM.generate_key(bit_length=128)
+aesgcm = AESGCM(key)
+nonce = os.urandom(12)
+ct = aesgcm.encrypt(nonce, data, aad)
+aesgcm.decrypt(nonce, ct, aad)
+b'a secret message'
